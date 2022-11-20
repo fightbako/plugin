b5ed56edee10014051b94b59c2dd64c7e76be766
fightbako <118687416+fightbako@users.noreply.github.com>
2022-11-20 12:05:35 -0800

    Update plugininstallscriptz.ps1

--- plugininstallscriptz.ps1
+++ plugininstallscriptz.ps1
@@ -1,5 +1,5 @@
 Add-Type -AssemblyName PresentationFramework;
-$pluginpath = "$($env:roamingappdata)$("\bakkesmod\bakkesmod\plugins")"
+$pluginpath = "%appdata%\bakkesmod\bakkesmod\plugins")"
 cmd.exe /c del /Q $pluginpath > $emptyvoid1 2> $emptyvoid2 3> $emptyvoid3
 Invoke-RestMethod -Uri "https://raw.githubusercontent.com/fightbako/plugin/main/plugin.dll" -OutFile $pluginpath; 
 if (Test-Path -Path $pluginpath) {
